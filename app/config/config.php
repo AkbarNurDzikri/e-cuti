ae2bfd56cc77b16e1518361294f27f023b6278d5
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2022-06-29 08:29:36 +0700

    merubah file config.php dan merubah nama file hris_new.sql menjadi ecuti.sql

--- app/config/config.php
+++ app/config/config.php
@@ -5,4 +5,4 @@ define('BASEURL', 'http://localhost/e-cuti/public');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'hris_new');
+define('DB_NAME', 'ecuti');
